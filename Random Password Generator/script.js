bcabae79dea79f162334cb898a0851601a3e08eb
Ankit2401 <au42884@gmail.com>
2025-07-01 23:12:39 +0530

    created javascript file to add functionality

---
+++ Random Password Generator/script.js
@@ -0,0 +1,12 @@
+const passwordBox = document.getElementById("password");
+const lenght = 10;
+
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_|}{[]></-=";
+
+function createPassword(){
+    
+}
