f576785c7fe4f58da4bf3ff571e6d61f708c9af6
Ankit2401 <au42884@gmail.com>
2025-07-02 22:45:17 +0530

    js is working and it is generating qr code

--- Qr Code Generator/script.js
+++ Qr Code Generator/script.js
@@ -1,8 +1,12 @@
-let imgBox = document.getElementById("imageBox")
-let qrImage = document.getElementById("qrImage ")
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText"); 
+
 
 
 
 function generateQR(){
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
      
 }
