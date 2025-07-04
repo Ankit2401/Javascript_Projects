854040ce4a602022abd289a3502231ce3e5ed6de
Ankit2401 <au42884@gmail.com>
2025-07-04 23:11:00 +0530

    applying logic to take data coming from api in web page

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,3 +1,5 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
 
 async function getquote(url){
