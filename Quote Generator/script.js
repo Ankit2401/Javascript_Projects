2e3dc7fa0f797016bd169b8d75bbf70ed4e96609
Ankit2401 <au42884@gmail.com>
2025-07-05 22:34:00 +0530

    taking data in frontend

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -5,7 +5,9 @@ const api_url = "https://api.quotable.io/random";
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
 }
 
 getquote(api_url);
